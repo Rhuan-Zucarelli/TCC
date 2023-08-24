8b6b93b4a63fb1f896c8ae18cd1a0263c5399d44
MuriloVetrin <murilovtrindade@gmail.com>
2023-08-24 10:25:49 -0300

    add logo on home page

--- botando-chape/src/components/layout/main/Main.tsx
+++ botando-chape/src/components/layout/main/Main.tsx
@@ -2,12 +2,13 @@ import React, { ReactNode } from "react";
 
 import { HiOutlineHome } from "react-icons/Hi";
 import { IoFastFoodOutline } from "react-icons/io5";
+import { PiForkKnifeDuotone } from "react-icons/Pi";
 import { CiDumbbell } from "react-icons/Ci";
 import { PiGearSixBold } from "react-icons/Pi";
 import { BiExit } from "react-icons/bi";
 import { useSession } from "next-auth/react";
-import { api } from "next/utils/api"
-import { signOut } from 'next-auth/react';
+import { api } from "next/utils/api";
+import { signOut } from "next-auth/react";
 import Login from "../login/Login";
 import Link from "next/link";
 
@@ -17,30 +18,30 @@ interface MainLayoutProps {
 
 export default function Main({ children }: MainLayoutProps) {
   const { data: sessionData } = useSession();
-  const user = api.user.getUser.useQuery({ id: sessionData?.user.id })
+  const user = api.user.getUser.useQuery({ id: sessionData?.user.id });
 
-  if (!sessionData) return <Login />
+  if (!sessionData) return <Login />;
   return (
     <div>
-      <div className="h-screen flex flex-col">
+      <div className="flex h-screen flex-col">
         <div className="flex flex-1">
-          <aside className="w-32 bg-green-600 flex flex-col justify-center items-center">
+          <aside className="flex w-32 flex-col items-center justify-center bg-green-600">
             <nav className="mb-12">
-              <button className="cursor-pointer focus:outline-none hover:bg-gray-500 hover:bg-opacity-50 rounded-2xl p-1">
+              <button className="cursor-pointer rounded-2xl p-1 hover:bg-gray-500 hover:bg-opacity-50 focus:outline-none">
                 <Link href="/">
                   <HiOutlineHome size={54} color="#fff" />
                 </Link>
               </button>
             </nav>
             <nav className="mb-12">
-              <button className="cursor-pointer focus:outline-none hover:bg-gray-500 hover:bg-opacity-50 rounded-2xl p-1">
+              <button className="cursor-pointer rounded-2xl p-1 hover:bg-gray-500 hover:bg-opacity-50 focus:outline-none">
                 <a href="#">
-                  <IoFastFoodOutline size={54} color="#fff" />
+                  <PiForkKnifeDuotone size={54} color="#fff" />
                 </a>
               </button>
             </nav>
             <nav className="mb-12">
-              <button className="cursor-pointer focus:outline-none hover:bg-gray-500 hover:bg-opacity-50 rounded-2xl p-1">
+              <button className="cursor-pointer rounded-2xl p-1 hover:bg-gray-500 hover:bg-opacity-50 focus:outline-none">
                 <a href="#">
                   <CiDumbbell size={54} color="#fff" />
                 </a>
@@ -48,31 +49,35 @@ export default function Main({ children }: MainLayoutProps) {
             </nav>
           </aside>
 
-          <main className="flex-1 flex items-center justify-center">
+          <main className="flex flex-1 items-center justify-center">
             {children}
           </main>
 
-          <aside className="w-32 bg-green-600 flex flex-col justify-center items-center">
+          <aside className="flex w-32 flex-col items-center justify-center bg-green-600">
 
-            <nav className="mb-12 flex-grow flex flex-col justify-end items-center">
+            <nav className="mb-6">
               <div className="flex-grow"></div>
-              <img src="" className="w-32 h-auto mb-4" />
-              <button className="cursor-pointer focus:outline-none hover:bg-gray-500 hover:bg-opacity-50 rounded-2xl p-1">
+              <img src="/imgs/LOGO.png" className="mb-4 mt-4 h-auto w-20" />
+            </nav>
+
+            <nav className="mb-6 flex flex-grow flex-col items-center justify-end">
+              <div className="flex-grow"></div>
+              <button className="cursor-pointer rounded-2xl p-1 hover:bg-gray-500 hover:bg-opacity-50 focus:outline-none">
                 <Link href="/configUser">
                   <PiGearSixBold size={54} color="#fff" />
                 </Link>
               </button>
             </nav>
 
-            <nav className="mb-12 ">
+            <nav className="mb-6 ">
               <div className="flex-grow"></div>
               <button
-                className="cursor-pointer focus:outline-none hover:bg-gray-500 hover:bg-opacity-50 rounded-2xl p-1"
-                onClick={() => signOut()}>
+                className="cursor-pointer rounded-2xl p-1 hover:bg-gray-500 hover:bg-opacity-50 focus:outline-none"
+                onClick={() => signOut()}
+              >
                 <BiExit size={54} color="#fff" />
               </button>
             </nav>
-
           </aside>
         </div>
       </div>
