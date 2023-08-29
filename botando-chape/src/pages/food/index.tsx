ea59955fe2b5eee5e124330e7a85ab676c1f8553
Rhuan Carlos Zucarelli <61522064+Rhuan-Zucarelli@users.noreply.github.com>
2023-08-29 00:37:29 -0300

    Made the modal component and implemented it on the food screen

--- botando-chape/src/pages/food/index.tsx
+++ botando-chape/src/pages/food/index.tsx
@@ -1,28 +1,55 @@
+import { Modal } from 'next/components/layout/modal';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { PiSunHorizon } from 'react-icons/Pi';
 import { PiSunDim } from 'react-icons/Pi';
 import { PiMoonStarsLight } from 'react-icons/Pi';
 
 
 export default function Food() {
+  const [openBreackFast, setOpenBreackFast] = useState(false);
+  const [openLunch, setOpenLunch] = useState(false);
+  const [openDinner, setOpenDinner] = useState(false);
+
+
 
   return (
     <>
       <div className="flex items-center justify-center h-screen">
         <div className="flex flex-col space-y-4">
-          <button className="bg-green-600 hover:bg-green-700 hover:scale-110 text-white  px-4 py-2 rounded-2xl p-1 flex items-center">
+          <button
+            className="bg-green-600 hover:bg-green-700 hover:scale-110 text-white  px-4 py-2 rounded-2xl p-1 flex items-center"
+            onClick={() => setOpenBreackFast(!openBreackFast)}
+          >
             <PiSunHorizon size={30} color='#fff' />
             <span className="ml-2">Café da manhã</span>
           </button>
-          <button className="bg-green-600 hover:bg-green-700 hover:scale-110 text-white px-4 py-2 rounded-2xl p-1 flex items-center">
+          <Modal isOpen={openBreackFast} onClose={() => setOpenBreackFast(!openBreackFast)}>
+
+          </Modal>
+
+          <button
+            className="bg-green-600 hover:bg-green-700 hover:scale-110 text-white px-4 py-2 rounded-2xl p-1 flex items-center"
+            onClick={() => setOpenLunch(!openLunch)}
+          >
             <PiSunDim size={30} color='#fff' />
             <span className="ml-2">Almoço</span>
           </button>
-          <button className="bg-green-600 hover:bg-green-700 hover:scale-110 text-white px-4 py-2 rounded-2xl p-1 flex items-center ">
+          <Modal isOpen={openLunch} onClose={() => setOpenLunch(!openLunch)}>
+
+          </Modal>
+
+          <button
+            className="bg-green-600 hover:bg-green-700 hover:scale-110 text-white px-4 py-2 rounded-2xl p-1 flex items-center "
+            onClick={() => setOpenDinner(!openDinner)}
+          >
             <PiMoonStarsLight size={30} color='#fff' />
             <span className="ml-2">Jantar</span>
           </button>
+          <Modal isOpen={openDinner} onClose={() => setOpenDinner(!openDinner)}>
+
+          </Modal>
+
         </div>
       </div>
 
